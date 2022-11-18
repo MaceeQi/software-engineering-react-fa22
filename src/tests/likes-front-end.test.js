0cfec978daac77809912bf41921f4804a59f88d5
MaceeQi <maceeqi@gmail.com>
2022-11-18 12:05:12 -0500

    added unit tests for mylikes and mydislikes screens

--- src/tests/likes-front-end.test.js
+++ src/tests/likes-front-end.test.js
@@ -1,6 +1,7 @@
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import TuitStats from "../components/tuits/tuit-stats";
+import Tuits from "../components/tuits";
 
 const MOCKED_USERS = [
     {username: 'alice', password: 'alice123', email: 'alice@wonderland.com', _id: "123"},
@@ -12,6 +13,32 @@ const MOCKED_TUITS = [
         stats: {replies: 5, retuits: 22, likes: 0, dislikes: 0}},
     {_id: "234", tuit: "bob's tuit", postedBy: MOCKED_USERS[1],
         stats: {replies: 5, retuits: 22, likes: 0, dislikes: 0}},
+    {_id: "345", tuit: "wonderland's second", postedBy: MOCKED_USERS[0],
+        stats: {replies: 5, retuits: 22, likes: 1, dislikes: 0}},
+    {_id: "456", tuit: "hope's 2nd", postedBy: MOCKED_USERS[1],
+        stats: {replies: 5, retuits: 22, likes: 1, dislikes: 0}},
+    {_id: "567", tuit: "test dislike", postedBy: MOCKED_USERS[1],
+        stats: {replies: 5, retuits: 22, likes: 0, dislikes: 1}},
+    {_id: "456", tuit: "test second", postedBy: MOCKED_USERS[0],
+        stats: {replies: 5, retuits: 22, likes: 0, dislikes: 1}},
+];
+
+const MOCKED_LIKES = [
+    {
+        _id: "123", tuit: MOCKED_TUITS[2], likedBy: MOCKED_USERS[0], type: "LIKED"
+    },
+    {
+        _id: "234", tuit: MOCKED_TUITS[3], likedBy: MOCKED_USERS[0], type: "LIKED"
+    },
+];
+
+const MOCKED_DISLIKES = [
+    {
+        _id: "345", tuit: MOCKED_TUITS[4], likedBy: MOCKED_USERS[1], type: "DISLIKED"
+    },
+    {
+        _id: "456", tuit: MOCKED_TUITS[5], likedBy: MOCKED_USERS[1], type: "DISLIKED"
+    },
 ];
 
 test('test liking - tuit stats likes +1, solid thumbs up rendered', () => {
@@ -82,5 +109,53 @@ test('test disliking - tuit stats dislikes +1, solid thumbs down rendered', () =
     expect(screen.getByText(/0/i)).toBeInTheDocument();
     expect(screen.getByTitle("solid thumbs down")).toBeInTheDocument();
     expect(screen.getByTitle("empty thumbs up")).toBeInTheDocument();
+});
+
+test('test mylikes screen', () => {
+    const findTuitsILike = () => {
+        const likes = MOCKED_LIKES;
+        const tuits = likes.map(like => like.tuit);
+        return tuits;
+    }
+
+    const likedTuits = findTuitsILike();
+
+    render(
+        <HashRouter>
+            <Tuits tuits={likedTuits}/>
+        </HashRouter>
+    );
+
+    // Check mylikes screen of tuits renders as expected
+    const solidThumbsUp = screen.getAllByTitle("solid thumbs up");
+    expect(solidThumbsUp.length).toEqual(2);
+    const emptyThumbsDown = screen.getAllByTitle("empty thumbs down");
+    expect(emptyThumbsDown.length).toEqual(2);
+    expect(screen.getByText(/wonderland's second/i)).toBeInTheDocument();
+    expect(screen.getByText(/hope's 2nd/i)).toBeInTheDocument();
+});
+
+test('test mydislikes screen', () => {
+    const findTuitsIDislike = () => {
+        const dislikes = MOCKED_DISLIKES;
+        const tuits = dislikes.map(dislike => dislike.tuit);
+        return tuits;
+    }
+
+    const dislikedTuits = findTuitsIDislike();
+
+    render(
+        <HashRouter>
+            <Tuits tuits={dislikedTuits}/>
+        </HashRouter>
+    );
+
+    // Check mydislikes screen of tuits renders as expected
+    const solidThumbsUp = screen.getAllByTitle("solid thumbs down");
+    expect(solidThumbsUp.length).toEqual(2);
+    const emptyThumbsDown = screen.getAllByTitle("empty thumbs up");
+    expect(emptyThumbsDown.length).toEqual(2);
+    expect(screen.getByText(/test dislike/i)).toBeInTheDocument();
+    expect(screen.getByText(/test second/i)).toBeInTheDocument();
 });
 
