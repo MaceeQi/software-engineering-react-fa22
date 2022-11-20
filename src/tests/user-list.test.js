7afb41c26ffe7e17a0e0a961f7284a1348b246de
MaceeQi <maceeqi@gmail.com>
2022-11-19 21:08:35 -0500

    changed references to id to _id to reflect updated user schema

--- src/tests/user-list.test.js
+++ src/tests/user-list.test.js
@@ -5,8 +5,8 @@ import {findAllUsers} from "../services/users-service";
 import axios from "axios";
 
 const MOCKED_USERS = [
-  {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', id: "123"},
-  {username: 'sarah_conor', password: 'illbeback', email: 'sarah@bigjeff.com', id: "234"},
+  {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
+  {username: 'sarah_conor', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"},
 ]
 
 test('user list renders static user array', () => {
